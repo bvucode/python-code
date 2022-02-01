0543c8c0a8d3ff9cdbd87b271843b424cbc47112
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2022-02-01 16:39:57 +0500

    Add files via upload

---
+++ orelreshka.py
@@ -0,0 +1,14 @@
+import random
+print("game orlel or reshka")
+def main():
+    guess = ""
+    xlist = ["reshka", "orel"]
+    while guess not in xlist:
+        guess = input("orel or reshka: ")
+        t = random.randint(0, 1)
+        if guess == xlist[t]:
+            print("yes")
+        else:
+            print("no")
+while True:
+    main()
