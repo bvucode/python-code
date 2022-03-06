f231246d59e80ccd17b4b1b963c3012eb2e31a57
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2022-03-07 02:17:34 +0500

    Add files via upload

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,5 +1,4 @@
-# generate and save password 
-# author Bulat
+# password generator
 
 import random
 import json
@@ -7,20 +6,11 @@ import json
 print("password generator and save")
 s1 = "abcdefghijklmnopqrstuvwxyz"
 s2 = "0123456789"
-dicw = {}
 def main():
     l = int(input("password length: "))
     generate(l)
 def save(x):
     print("password is {}".format(x))
-    inp_q = input("save ? (y/n) ")
-    if inp_q == "y":
-        n = input("web-site: ")
-        dicw[n] = x
-        with open("passwords.json", "a") as file:
-            json.dump(dicw, file)
-            del dicw[n]
-        print("save!")
 def generate(x):
     p1 = ""
     p2 = random.choice(s2)
