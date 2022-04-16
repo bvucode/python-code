08106276025747f918e4ca497f2a955be60864c4
vbucode <61677060+vbucode@users.noreply.github.com>
2022-04-16 23:29:42 +0500

    Add files via upload

---
+++ polyglot.py
@@ -0,0 +1,38 @@
+import random
+import re
+import json
+
+
+data = """{"i will not travel in winter":"я не буду путешествовать зимой",
+     "what are you doing next week?":"что ты делаешь на следующей неделе?",
+     "what do you think?":"что скажешь?",
+     "how long have you know her?":"как давно вы с ней знакомы?",
+     "is this your cellphone?":"это твой телефон?"}"""
+
+d = json.loads(data)
+
+def main():
+    count = 0
+    engvar, ruvar = random.choice(list(d.items()))
+    print("\n{}".format(ruvar))
+    wlist = []
+    wlist = re.split("[\W\s]", engvar) 
+    for i in wlist:
+        print(i)
+    tr = input("\ntranslate: ")
+    if tr == "exit":
+        leave()
+    wlist2 = []
+    wlist2 = re.split("[\W\s]", tr)
+    for i in wlist2:
+        count += 1
+        if i != wlist[count-1]:
+            print("\nright answer: {}".format(engvar))
+            break
+        elif count == len(wlist):
+            print("\ngood!")
+
+def leave():
+    exit()
+while True:
+    main()
