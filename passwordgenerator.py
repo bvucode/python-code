b14823808c2bf99626251e37045d62cd375e186b
vbucode <61677060+vbucode@users.noreply.github.com>
2022-04-19 21:14:48 +0500

    Add files via upload

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,26 +1,25 @@
 # password generator
 
 import random
-import json
 
 print("password generator and save")
 s1 = "abcdefghijklmnopqrstuvwxyz"
 s2 = "0123456789"
+s3 = "@!.,"
 def main():
     l = input("password length: ")
     if l == "exit":
         leave()
     generate(int(l))
-def save(x):
-    print("password is {}".format(x))
 def generate(x):
     p1 = ""
     p2 = random.choice(s2)
-    for i in range(x-1):
+    p3 = random.choice(s3)
+    for i in range(x-2):
         p1 += random.choice(s1)
-    p = p1 + p2
+    p = p1 + p2 + p3
     u = p.capitalize()
-    save(u)
+    print("password is {}".format(u))
 def leave():
     exit()
 while True:
