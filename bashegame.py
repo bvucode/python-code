f27762ee4a27d819d39b49cd63a5dfbd00c1fd48
vbucode <61677060+vbucode@users.noreply.github.com>
2022-05-02 18:18:52 +0500

    Add files via upload

--- bashegame.py
+++ bashegame.py
@@ -1,5 +1,3 @@
-# Bashe Game
-
 import random
 
 print('Game Bashe. There are 11 items. Rivals go in turn, for each move \nany of the players can take 1, 2 or 3 items. \nLoses the one who is forced to take the last item.')
@@ -12,50 +10,43 @@ def main():
     global things
     user = int(input('Enter: number 1, 2 or 3: '))
     things -= user
-    logic(user, things)
+    vl = logic(user)
+    if things == 1:
+        things -= vl
+        printr(vl, things)
+    else:
+        things -= vl
+        printr(vl, things)
+
 def printr(apptake, targ):
     global things
-    global first
     if targ == 0:
         print("You win!")
         things = 11
-        first = 0
     elif targ == 1:
         print("You lost! Total left {}".format(targ))
         things = 11
-        first = 0
     else:
         print("The app took {}. Total left {}".format(apptake, targ))
         things = targ
-def logic(usertake, targ):
+
+def logic(xarg):
+    global things
     global first
-    if targ > 1:
+    if things > 1:
         if first == 0:
-            if usertake == 1:
-                first = 1
-                apptake = 1
-                targ -= apptake
-                printr(apptake, targ)
-            elif usertake == 2:
-                first = 1
-                apptake = random.randint(1, 3)
-                targ -= apptake
-                printr(apptake, targ)
-            elif usertake == 3:
-                first = 1
-                apptake = 3
-                targ -= apptake
-                printr(apptake, targ)
+            if xarg == 2:
+                xarg = random.randint(1, 3)
+            else:
+                xarg = 4 - xarg
+            first = 1
+        if things % 2 == 0:
+            xarg = 1
         else:
-           apptake = winalgorithm(usertake)
-           targ -= apptake
-           printr(apptake, targ)
-    elif targ == 1:
-        apptake = 1
-        targ -= apptake
-        printr(apptake, targ)
-def winalgorithm(usertake):
-    var = 4 - usertake
-    return var
+            xarg = 4 - xarg
+    else:
+        xarg = 1
+    return xarg
+
 while True:
     main()
