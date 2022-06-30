d7401baac0579408d1e7927aa8764adaee8a254e
vbucode <61677060+vbucode@users.noreply.github.com>
2022-06-30 12:15:05 +0500

    Add files via upload

--- bashegame.py
+++ bashegame.py
@@ -1,56 +1,52 @@
 import random
 
-print('Game Bashe. There are 11 items. Rivals go in turn, for each move \nany of the players can take 1, 2 or 3 items. \nLoses the one who is forced to take the last item.')
-print('Игра Баше. 11 предметов. Соперники ходят по очереди, за каждый ход \nлюбой  из играющих может взять 1, 2 или 3 предмета. \nПроигрывает тот, кто вынужден \nвзять последний предмет.')
-
-things = 11 # 11 things. The game can have 7, 11, 15, 19, 21, ... things.
-first = 0
+THINGS = 11 # 11 things. The game can have 7, 11, 15, 19, 21, ... things.
 
 def main():
-    global things
-    user = int(input('Enter: number 1, 2 or 3: '))
-    things -= user
-    vl = logic(user)
-    if things == 1:
-        things -= vl
-        printr(vl, things)
-    elif things == 0:
-        printr(vl, things)
-    else:
-        things -= vl
-        printr(vl, things)
-
-def printr(apptake, targ):
-    global things
-    print("The app took {}. Total left {}".format(apptake, targ))
-    if targ == 0:
-        print("You win!")
-        things = 11
-    elif targ == 1:
-        print("You lost! Total left {}".format(targ))
-        things = 11 
-
-def logic(xarg):
-    global things
-    global first
-    if things > 1:
-        if first == 0:
-            if xarg == 2:
-                xarg = random.randint(1, 3)
-            else:
-                xarg = 4 - xarg
-            first = 1
-        if things == 7:
-            xarg = 2
-        elif things == 4:
-            xarg = 3
-        elif things % 2 == 0:
-            xarg = 1
+    things = THINGS #make same number THINGS
+    firststep = 0
+    print("""Game Bashe. There are 11 items. Rivals go in turn, for each move
+any of the players can take 1, 2 or 3 items.
+Loses the one who is forced to take the last item.
+Игра Баше. 11 предметов. Соперники ходят по очереди, за каждый ход
+любой  из играющих может взять 1, 2 или 3 предмета.
+Проигрывает тот, кто вынужден взять последний предмет.""")
+    while True:
+        user = int(input('Enter: number 1, 2 or 3: '))
+        if user > 3:
+            print("try again")
         else:
-            xarg = 4 - xarg
-    else:
-        xarg = 1
-    return xarg
-
-while True:
+            things -= user
+            if firststep == 0:
+                if user == 2:
+                    apptake = random.randint(1, 3)
+                    firststep = 1
+                elif user == 1:
+                    if THINGS == 7 or THINGS == 11:
+                        apptake = 1
+                    else:
+                        apptake = 4 - user
+                elif user != 2:
+                    apptake = 4 - user
+                    firststep = 1
+            elif things == 7:
+                apptake = 2
+            elif things <= 3:
+                apptake = things
+            elif things != 7 and user == 4 - apptake:
+                apptake = random.randint(1, 3)
+            elif things != 7 and user != 4 - apptake:
+                apptake = 4 - user
+            things -= apptake
+        print("The app took {}. Total left {}".format(apptake, things))
+        if things == 0:
+            print("You win!")
+        elif things == 1:
+            print("You lost! Total left {}".format(things))
+        if things == 0 or things == 1:
+            print("Do you want play again? (y or n)")
+            if not input("> ").lower().startswith("y"):
+                break
+            print("Thanks for playing!")
+if __name__ == "__main__":
     main()
