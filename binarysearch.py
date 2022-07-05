e22a16dc02c1ab2204d413d76a8b7b0643022531
vbucode <61677060+vbucode@users.noreply.github.com>
2022-07-05 21:06:58 +0500

    Add files via upload

---
+++ binarysearch.py
@@ -0,0 +1,17 @@
+def binarysearch(xlist, item):
+    low:int = 0
+    high:int = len(xlist)-1
+
+    while low <= high:
+        mid:int = (low + high) // 2
+        if xlist[mid] == item:
+            return mid
+        elif xlist[mid] < item:
+            low = mid + 1
+        elif xlist[mid] > item:
+            high = mid - 1
+        else:
+            return True
+    return False
+print(binarysearch(["a", "d", "e", "f", "x"], "f"))
+    
