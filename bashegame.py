aacaf7a4c8b08abceea77044311907ea0481dae2
vbu <61677060+vbucode@users.noreply.github.com>
2023-04-22 22:09:35 +0500

    Add files via upload

--- bashegame.py
+++ bashegame.py
@@ -1,37 +1,33 @@
 import random
 
-THINGS = 11 # 11 things. The game can have 7, 11, 15, 19, 21, ... things.
-
 def main():
-    things = THINGS #make same number THINGS
+    # The game can have 7, 11, 15, 19, 21, ... things.
+    THINGS = 11
+    things = THINGS
     firststep = 0
-    print("""Game Bashe. There are 11 items. Rivals go in turn, for each move any of the players can take 1, 2 or 3 items. Loses the one who is forced to take the last item. Игра Баше. 11 предметов. Соперники ходят по очереди, за каждый ход любой  из играющих может взять 1, 2 или 3 предмета. Проигрывает тот, кто вынужден взять последний предмет.""")
+    winalg = 0
+    print("""Game Bashe. There are 11 items. Rivals go in turn, \nfor each move any of the players can take 1, 2 or 3 items. \nLoses the one who is forced to take the last item.
+             \nИгра Баше. 11 предметов. Соперники ходят по очереди, за каждый ход любой  из играющих может взять 1, 2 или 3 предмета. \nПроигрывает тот, кто вынужден взять последний предмет.\n""")
     while True:
         user = int(input('Enter: number 1, 2 or 3: '))
-        if user > 3:
+        if user > 3 :
             print("try again")
         else:
             things -= user
-            if firststep == 0:
-                if user == 2:
-                    apptake = random.randint(1, 3)
-                    firststep = 1
-                elif user == 1:
-                    if THINGS == 7 or THINGS == 11:
+            if firststep == 0 and user != 2:
+                apptake = user
+                winalg = 1
+                firststep = 1
+            else:
+                if winalg == 1:
+                    apptake = 4 - user
+                elif winalg == 1 and things == 2:
+                    apptake = 1
+                else:
+                    if things == 1:
                         apptake = 1
                     else:
-                        apptake = 4 - user
-                elif user != 2:
-                    apptake = 4 - user
-                    firststep = 1
-            elif things == 7:
-                apptake = 2
-            elif things <= 3:
-                apptake = things
-            elif things != 7 and user == 4 - apptake:
-                apptake = random.randint(1, 3)
-            elif things != 7 and user != 4 - apptake:
-                apptake = 4 - user
+                        apptake = random.randint(1, 3)
             things -= apptake
         print("The app took {}. Total left {}".format(apptake, things))
         if things == 0:
@@ -44,5 +40,6 @@ def main():
                 break
             else:
                 print("Thanks for playing!")
+                things = THINGS
 if __name__ == "__main__":
     main()
