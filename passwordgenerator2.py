224fe62c203d82eacdd4e33921dbabd4ff31bfe5
Bulat Valiakhmetov <61677060+vbucode@users.noreply.github.com>
2023-05-10 04:37:47 +0500

    Add files via upload

--- passwordgenerator2.py
+++ passwordgenerator2.py
@@ -1,16 +1,17 @@
 import random
 
 def generate_passwords(start_len, end_len):
+    quantity = 100000
     charn = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
     passwords = []
     for i in range(start_len, end_len + 1):
-        while len(passwords) < 1000:
+        while len(passwords) < quantity:
             password = ""
             for j in range(i):
                 password += random.choice(charn)
             if password not in passwords:
                 passwords.append(password)
-            if len(passwords) == 1000:
+            if len(passwords) == quantity:
                 print(f'{i} символьные пароли сгенерированы.')
                 with open(f'passwords.txt', 'a') as f:
                     for password in passwords:
