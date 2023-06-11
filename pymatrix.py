eb75345c123f0d19effc2de06ae267fc75f17146
Bulat Valiakhmetov <61677060+vbucode@users.noreply.github.com>
2023-06-11 17:58:49 +0500

    Add files via upload

--- pymatrix.py
+++ pymatrix.py
@@ -1,8 +1,22 @@
 import random, time
 print("\x1b[2J", end = "")
-char = "abcdifg"
+char = " abcdifg "
 while True:
     print('\x1b[H', end='')
+    flag = 0
+    c = []
     for i in range(20):
-        c = [random.choice(char) for i in range(20)]
-        print(" ".join(c))
+        while flag == 0:
+            c = [random.choice(char) for i in range(20)]
+            print(" ".join(c))
+            flag = 1
+        else:
+            xlist = []
+            for l in c:
+                if l == " ":
+                    xlist.append(" ")
+                else:
+                    c2 = random.choice(char)
+                    xlist.append(c2)
+            print(" ".join(xlist))
+    time.sleep(0.5)
