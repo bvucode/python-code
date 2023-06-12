896d9e762b8d48d984b4ccac9b9c82a8c81d132a
Bulat Valiakhmetov <61677060+vbucode@users.noreply.github.com>
2023-06-13 00:30:32 +0500

    Add files via upload
    matrix effect
    final version

--- pymatrix.py
+++ pymatrix.py
@@ -1,42 +1,56 @@
 import random, time
 print("\x1b[2J", end = "")
 char = " a b c d e f g h i j k l m n "
-char2 = " .,/)(><^?_+-%':&][#$!abcdefghijklmnopqrstuvwxyzABCDIFGHIJKLMNOPQRSTUVWXYZ01234 56789 "
+char2 = ".,/)(><^?_+-%':&][#$!abcdefghijklmnopqrstuvwxyzABCDIFGHIJKLMNOPQRSTUVWXYZ0123456789"
 while True:
     print('\x1b[H', end='')
     flag = 0
     tlist = []
-    for j in range(20):
+    c = []
+    for x, j in enumerate(range(21)):
         while flag == 0:
-            c = [random.choice(char) for i in range(40)]
-            tlist.append(c)
-            print(" ".join(c))
+            c = [(random.choice(char), random.randint(1, 10)) for i in range(40)]
             flag = 1
         else:
             xlist = []
             for l in c:
-                if l == " ":
+                if l[0] == " ":
                     xlist.append(" ")
                 else:
-                    c2 = random.choice(char2)
+                    if l[1] < x:
+                        c2 = " "
+                    else:
+                        c2 = random.choice(char2)
                     xlist.append(c2)
             print(" ".join(xlist))
             tlist.append(xlist)
-    time.sleep(0.01)
+    time.sleep(0.15)
     print('\x1b[H', end='')
-    for i in range(15):
-        for y, j in enumerate(tlist):
+    for y, j in enumerate(tlist):
+        if y == len(tlist) - 3:
+            break
+        else:
             for x, k in enumerate(j):
                 if k != " ":
-                    for l in range(10 - (y + 1)):
-                        try:
-                            if tlist[l - 1][x] == " ":
-                                tlist[l - 1][x] = random.choice(char2)
+                    if c[x][1] >= 5:
+                        for l in range(len(tlist) - (y + 1)):
+                            try:
+                                if tlist[y + l][x] == " ":
+                                    tlist[y + l][x] = random.choice(char2)
+                                    break
+                            except IndexError:
                                 break
-                        except IndexError:
-                            pass
-        for i in tlist:
-            print(" ".join(i))
-        time.sleep(0.01)
-        print('\x1b[H', end='')
+                    else:
+                        for l in range(len(tlist) - (y + 1)):
+                            try:
+                                if tlist[y + l][x] == " ":
+                                    tlist[y + l][x] = random.choice(char2)
+                                    j[x] = " "
+                                    break
+                            except IndexError:
+                                break
+            for i in tlist:
+                print(" ".join(i))
+            time.sleep(0.16)
+            print('\x1b[H', end='')
     
