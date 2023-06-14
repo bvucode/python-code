5b1ae3a89feb986a557570fd54469687f5fd08a9
Bulat Valiakhmetov <61677060+vbucode@users.noreply.github.com>
2023-06-14 13:03:16 +0500

    Add files via upload

--- pymatrix.py
+++ pymatrix.py
@@ -1,13 +1,15 @@
 import random, time
 print("\x1b[2J", end = "")
 char = ".,/)(><^?_+-%':&][#$!abcdefghijklmnopqrstuvwxyzABCDIFGHIJKLMNOPQRSTUVWXYZ0123456789"
+W = 104
+H = 51
 while True:
     print('\x1b[H', end='')
     flag = 0
     tlist = []
-    for x, j in enumerate(range(51)):
+    for x, j in enumerate(range(H)):
         while flag == 0:
-            c = [(random.choice("01"), random.randint(1, 50)) for i in range(104)]
+            c = [(random.choice("01"), random.randint(1, 50)) for i in range(W)]
             flag = 1
         else:
             xlist = []
@@ -22,12 +24,12 @@ while True:
                     xlist.append(c2)
             tlist.append(xlist)
     for y, j in enumerate(tlist):
-        if y == len(tlist) - 10:
+        if y == len(tlist) - 5:
             break
         else:
             for x, k in enumerate(j):
                 if k != " ":
-                    if c[x][1] >= 15:
+                    if c[x][1] >= 25:
                         for l in range(len(tlist) - (y + 1)):
                             try:
                                 if tlist[y + l][x] == " ":
@@ -40,7 +42,7 @@ while True:
                             if c[x + 1][1] > c[x][1]:
                                 for l in range(len(tlist) - (y + 1)):
                                     try:
-                                        if c[x - 1][0] == "0" and y > 10:
+                                        if c[x - 1][0] == "0" and y > 25:
                                             tlist[l][x - 1] = random.choice(char)
                                         if tlist[y + l][x] == " ":
                                             tlist[y + l][x] = random.choice(char)
