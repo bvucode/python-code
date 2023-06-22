55761c1e05ea7749b314de0aad908cfd40f06360
Bulat Valiakhmetov <61677060+vbucode@users.noreply.github.com>
2023-06-23 01:48:44 +0500

    Add files via upload

--- pymatrix.py
+++ pymatrix.py
@@ -2,34 +2,50 @@ import random, time
 print("\x1b[2J", end = "")
 tlist = []
 flag = 0
-char = ".,/)(><^?_+-%':&][#$!abcdefghijklmnopqrstuvwxyzABCDIFGHIJKLMNOPQRSTUVWXYZ0123456789"
 W = 104
 H = 50
+char = ".,/)(><^?_+-%':&][#$!abcdefghijklmnopqrstuvwxyzABCDIFGHIJKLMNOPQRSTUVWXYZ0123456789"
 while True:
     print("\x1b[H", end="")
-    c = [(random.randint(0, 1), random.randint(1, H)) for i in range(W)]
     if flag == 0:
+        cf = [(random.randint(0, 3), random.randint(5, H)) for i in range(W)]
         for i in range(H):
             c = [" " for i in range(W)]
             tlist.append(c)
+        for y, j in enumerate(tlist[0]):
+            if cf[y][0] == 1:
+                for i in range(cf[y][1]):
+                    tlist[i][y] = random.choice(char)
         flag = 1
     for y, j in enumerate(tlist):
         for x, k in enumerate(j):
-            if c[x][0] == 1:
-                for l in range(len(tlist)):
-                    try:
-                        if tlist[y + (l + 1)][x] == " ":
-                            tlist[y + (l + 1)][x] = random.choice(char)
+            if k != " ":
+                try:
+                    for i in range(H):
+                        if tlist[y + i][x] == " ":
+                            tlist[y + i][x] = random.choice(char)
+                            j[x] = " "
+                            try:
+                                if tlist[y][x + 1] == " " and random.randint(1, 10) == 1:
+                                    rn = random.randint(5, H)
+                                    for l in range(rn):
+                                        tlist[l][x + 1] = random.choice(char)
+                            except IndexError:
+                                pass
                             break
-                    except IndexError:
-                        j[x] = " "
-            try:
-                if y + 1 >= c[x][1] and c[x][0] == 1:
-                    j[x] = random.choice(char)
+                except IndexError:
                     j[x] = " "
-            except IndexError:
-                pass
-        for i in tlist:
-            print(" ".join(i))
-        time.sleep(0.025)
-        print("\x1b[H", end="")
+                    tlist[-1][x] = " "
+                    """
+                try:
+                    if tlist[y][x + 1] == " ":
+                        rn = random.randint(1, H // 2)
+                        for l in range(rn):
+                            tlist[l][x + 1] = random.choice(char)
+                except IndexError:
+                    pass
+                    """
+            for i in tlist:
+                print(" ".join(i))
+            time.sleep(0.00001)
+            print("\x1b[H", end="")
