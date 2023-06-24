dc7b270cd5c39928fb3c5e30e62a9a9f95c98da6
Bulat Valiakhmetov <61677060+vbucode@users.noreply.github.com>
2023-06-25 00:53:58 +0500

    Add files via upload
    matrix effect final version

--- pymatrix.py
+++ pymatrix.py
@@ -7,45 +7,29 @@ H = 50
 char = ".,/)(><^?_+-%':&][#$!abcdefghijklmnopqrstuvwxyzABCDIFGHIJKLMNOPQRSTUVWXYZ0123456789"
 while True:
     print("\x1b[H", end="")
+    start = 0
     if flag == 0:
-        cf = [(random.randint(0, 3), random.randint(5, H)) for i in range(W)]
+        cf = [(random.randint(1, H), random.randint(1, H + 25)) for i in range(W)]
         for i in range(H):
             c = [" " for i in range(W)]
             tlist.append(c)
-        for y, j in enumerate(tlist[0]):
-            if cf[y][0] == 1:
-                for i in range(cf[y][1]):
-                    tlist[i][y] = random.choice(char)
+        for x, i in enumerate(tlist):
+            for y, j in enumerate(i):
+                if cf[y][0] >= x and cf[y][1] - x <= x:
+                   tlist[x][y] = random.choice(char)
         flag = 1
-    for y, j in enumerate(tlist):
-        for x, k in enumerate(j):
-            if k != " ":
-                try:
-                    for i in range(H):
-                        if tlist[y + i][x] == " ":
-                            tlist[y + i][x] = random.choice(char)
-                            j[x] = " "
-                            try:
-                                if tlist[y][x + 1] == " " and random.randint(1, 10) == 1:
-                                    rn = random.randint(5, H)
-                                    for l in range(rn):
-                                        tlist[l][x + 1] = random.choice(char)
-                            except IndexError:
-                                pass
-                            break
-                except IndexError:
-                    j[x] = " "
-                    tlist[-1][x] = " "
-                    """
-                try:
-                    if tlist[y][x + 1] == " ":
-                        rn = random.randint(1, H // 2)
-                        for l in range(rn):
-                            tlist[l][x + 1] = random.choice(char)
-                except IndexError:
-                    pass
-                    """
-            for i in tlist:
-                print(" ".join(i))
-            time.sleep(0.00001)
-            print("\x1b[H", end="")
+    for i in range(W):
+        for j in range(H):
+            try:
+                if tlist[j][i] != " " and start == 0:
+                    tlist[j][i] = " "
+                    start = 1
+                elif tlist[j][i] == " " and start == 1:
+                    tlist[j][i] = random.choice(char)
+                    start = 0
+            except IndexError:
+                tlist[j][i] = " "
+        for i in tlist:
+            print(" ".join(i))
+        time.sleep(0.001)
+        print("\x1b[H", end="")
